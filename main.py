a6a3c889bf253e497f6bd4a52ddc5594e1075d2f
shriad <shria.devarakonda@exponentialai.com>
2022-04-21 19:47:56 +0530

    added initializer, made it pep perfect

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import json
 from utils.util import input_taker, check_in, check_out
 
+
 class Morse:
     f = open("morse-code.json")
     morse_dict = json.load(f)
     f.close()
+
     def __init__(self, type_morse, val):
         self.val = val
         self.type_morse = type_morse
 
     def converter(self):
         if self.type_morse == "morse":
-            return self.convert(self.val,type_morse="morse")
+            return self.convert(self.val, type_morse="morse")
         elif self.type_morse == "text":
             return self.convert(self.val, type_morse="text")
         else:
-            raise Exception("please give input as 'morse' or 'text' for the needed conversion.")
+            raise Exception("Please give input as 'morse' or 'text' for the needed conversion.")
 
-    def convert(self, val, type_morse):
-        if type_morse== "text":
+    @staticmethod
+    def convert(val, type_morse):
+        if type_morse == "text":
             val = list(val)
             for i in range(len(val)):
                 if val[i] in Morse.morse_dict.keys():
-                    val[i]= Morse.morse_dict[val[i]]
+                    val[i] = Morse.morse_dict[val[i]]
                 else:
-                    val[i]=" "
+                    val[i] = " "
             return ' '.join(val)
         else:
             val = val.strip(' ').split(' ')
@@ -40,8 +43,10 @@ class Morse:
             return ' '.join(val.split())
 
 
-type_morse, val, out_type, in_type, in_file = input_taker()
-lines = check_in(in_file,val)
-morse_made = Morse(type_morse, lines)
-converted = morse_made.converter()
-check_out(out_type, converted, type_morse)
+def init():
+    type_morse, val, out_type, in_type, in_file = input_taker()
+    val = val.lower()
+    lines = check_in(in_file, val)
+    morse_made = Morse(type_morse, lines)
+    converted = morse_made.converter()
+    check_out(out_type, converted, type_morse)
--- utils/util.py
+++ utils/util.py
@@ -1,35 +1,36 @@
-def check_out(out_,converted,type_morse):
+def check_out(out_, converted, type_morse):
     if "y" in out_:
         from datetime import datetime
-        with open(f"file_from{type_morse}_{datetime.now().strftime('%H:%M:%S_%d:%m:%Y')}.txt","w+") as fi:
+        with open(f"file_from{type_morse}_{datetime.now().strftime('%H:%M:%S_%d:%m:%Y')}.txt", "w+") as fi:
             fi.write(converted)
-        print("File Updated")
+        print("File Updated\n")
     else:
         print(converted)
 
-def check_in(in_file,val):
+
+def check_in(in_file, val):
     if in_file:
         try:
-            with open(f"{in_file}","r") as fi:
+            with open(f"{in_file}", "r") as fi:
                 strs = fi.readlines()
-        except:
+        except Exception:
             raise Exception("File does not exist! Have you added the extension to the file name too? "
-                            "Or checked the path?")
+                            "Or checked the path?\n")
         return '\n'.join(strs)
     else:
         return val
 
 
 def input_taker():
-    type_morse_ = input(f"Type 'morse' if you wish to convert from morse to text\n"
-                       "Type 'text' if you wish to convert text to morse.\n").lower()
-    in_type = input("Would you require your input read from a file? 'y' for yes and 'n' for no\n").lower()
+    type_morse_ = input(f"Type 'morse' if you wish to convert from morse to text.\n"
+                        "Type 'text' if you wish to convert text to morse.\n").lower()
+    in_type = input("Would you require your input read from a file? 'y' for yes and 'n' for no.\n").lower()
     val_ = None
     if "y" not in list(in_type):
-        val_ = input("Enter your string to perform the operation on\n")
-    out_type = input("Would you require your output in a file? 'y' for yes and 'n' for no\n").lower()
-    in_file=None
+        val_ = input("Enter your string to perform the operation on.\n")
+    out_type = input("Would you require your output in a file? 'y' for yes and 'n' for no.\n").lower()
+    in_file = None
     if "y" in list(in_type):
-        in_file = input("Please provide filename of the file to read.").lower()
-    print("Any occurrance of '*' in the output signifies that the character could not be converted.")
-    return type_morse_, val_, out_type,in_type,in_file
+        in_file = input("Please provide filename of the file to read.\n").lower()
+    print("Any occurrence of '*' in the output signifies that the character could not be converted.")
+    return type_morse_, val_, out_type, in_type, in_file

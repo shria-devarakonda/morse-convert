b0c102242486030a61b4f77ae4df3a1133880b6e
shriad <shria.devarakonda@exponentialai.com>
2022-04-21 13:40:36 +0530

    updated for morse

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Morse:
+    f = open("morse-code.json")
+    morse_dict = json.load(f)
+    f.close()
+    def __init__(self, type_morse, val):
+        self.val = val
+        self.type_morse = type_morse
+
+    def converter(self):
+        if self.type_morse == "morse":
+            return self.convert(self.val,type_morse="morse")
+        elif self.type_morse == "text":
+            return self.convert(self.val, type_morse="text")
+        else:
+            raise Exception("please give input as 'morse' or 'text' for the needed conversion.")
+
+    def convert(self, val, type_morse):
+        if type_morse== "text":
+            val = list(val)
+            for i in range(len(val)):
+                if val[i] in Morse.morse_dict.keys():
+                    val[i]= Morse.morse_dict[val[i]]
+                else:
+                    val[i]=" "
+            return ' '.join(val)
+        else:
+            val = val.strip(' ').split(' ')
+            key_list = list(Morse.morse_dict.keys())
+            val_list = list(Morse.morse_dict.values())
+            for i in range(len(val)):
+                if val[i] in val_list:
+                    pos = val_list.index(val[i])
+                    val[i] = key_list[pos]
+                else:
+                    val[i] = " "
+            return ''.join(val)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def input_taker():
+    type_morse_ = input(f"Type 'morse' if you wish to convert from morse to text\n"
+                       "Type 'text' if you wish to convert text to morse.\n").lower()
+    val_ = input("Enter your string to perform the operation on\n")
+    return type_morse_, val_
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+type_morse, val = input_taker()
+morse_made = Morse(type_morse, val)
+converted = morse_made.converter()
+print(converted)
